4666beb3ff0c394cb24761b0d9bdf935ffa28d41
cilvin <cilvin33@users.noreply.github.com>
2019-09-21 09:14:43 -0700

    Updates

---
+++ precache-manifest.58a46a0d8dbb80c2809ff87cb0fbb2b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0967406b87618a0b49e1c630d5b9a46c",
+    "url": "/Movie-App-React/index.html"
+  },
+  {
+    "revision": "fd05225a54ced43aa348",
+    "url": "/Movie-App-React/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4afc8683d08ff58725e3",
+    "url": "/Movie-App-React/static/css/main.44d6910c.chunk.css"
+  },
+  {
+    "revision": "fd05225a54ced43aa348",
+    "url": "/Movie-App-React/static/js/2.71712545.chunk.js"
+  },
+  {
+    "revision": "4afc8683d08ff58725e3",
+    "url": "/Movie-App-React/static/js/main.0ee0f91f.chunk.js"
+  },
+  {
+    "revision": "1efd3b26681aa29d7a1f",
+    "url": "/Movie-App-React/static/js/runtime~main.08be0b7c.js"
+  }
+]);
